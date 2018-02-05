173c175e024743ded8406f9f3abd1339ecca508d
Alexei Verbitki <mailverbitski@gmail.com>
2018-02-05 09:20:15 +0200

    new

---
+++ src/Java_How_to_Programm_Early_Objects_Paul_Deitel/Chapter_17_Lambda/Exercises_17/Summarizing_the_Characters_in_a_File_17_9/StreamOfLines.java
@@ -0,0 +1,46 @@
+package Java_How_to_Programm_Early_Objects_Paul_Deitel.Chapter_17_Lambda.Exercises_17.Summarizing_the_Characters_in_a_File_17_9;// Fig. 17.17: StreamOfLines.java
+/*
+17.9 (Summarizing the Characters in a File) Modify the program of Fig. 17.17 to summarize
+the number of occurrences of every character in the file.
+*/
+
+// Counting word occurrences in a text file.
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class StreamOfLines
+{
+   public static void main(String[] args) throws IOException
+   {
+      // Regex that matches one or more consecutive whitespace characters
+      Pattern pattern = Pattern.compile("");
+
+      // count occurrences of each word in a Stream<String> sorted by word
+      Map<String, Long> wordCounts = 
+         Files.lines(Paths.get("/Users/Alexei/Google Drive/JAVA/src/Java_How_to_Programm_Early_Objects_Paul_Deitel/Chapter_17_Lambda/Exercises_17/Summarizing_the_Characters_in_a_File_17_9/Chapter2Paragraph.txt"))
+//              .map(line -> line.replaceAll("\\p{Punct}", ""))
+              .flatMap(line -> pattern.splitAsStream(line))
+              .collect(Collectors.groupingBy(String::toLowerCase,
+                 TreeMap::new, Collectors.counting()));
+      
+      // display the words grouped by starting letter
+      wordCounts.entrySet()
+         .stream()
+         .collect(
+            Collectors.groupingBy(entry -> entry.getKey().charAt(0),
+               TreeMap::new, Collectors.toList()))
+         .forEach((letter, wordList) -> 
+            { 
+               System.out.printf("%n%C%n", letter);
+               wordList.stream().forEach(word -> System.out.printf(
+                  "%13s: %d%n", word.getKey(), word.getValue()));
+            });
+   }
+} // end class StreamOfLines
+
+
---
+++ src/Java_How_to_Programm_Early_Objects_Paul_Deitel/Chapter_17_Lambda/Exercises_17/Summarizing_the_Characters_in_a_File_17_9/Summarizing_the_File_Types_in_a_Directory_17_10/Summarizing_the_File_Types_in_a_Directory_17_10.java
@@ -0,0 +1,59 @@
+package Java_How_to_Programm_Early_Objects_Paul_Deitel.Chapter_17_Lambda.Exercises_17.Summarizing_the_Characters_in_a_File_17_9.Summarizing_the_File_Types_in_a_Directory_17_10;
+/*
+17.10 (Summarizing the File Types in a Directory) Section 15.3 demonstrated how to get information
+about files and directories on disk. In addition, you used a DirectoryStream to display the
+contents of a directory. Interface DirectoryStream now contains default method entries, which
+returns a Stream. Use the techniques from Section 15.3, DirectoryStream method entries, lambdas
+and streams to summarize the types of files in a specified directory.*/
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SecureDirectoryStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Scanner;
+import java.nio.file.Files;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+
+public class Summarizing_the_File_Types_in_a_Directory_17_10 {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        Path path = Paths.get("/Users/Alexei/Downloads");
+        String[] i = new String[4];
+
+//        Map<String, Long> wordCounts = Files.lines(Paths.get("/Users/Alexei/Downloads"))
+        Stream<Path> entries = Files.list(path);
+        Stream<Path> test = Files.list(path);
+        Stream<Path> test1 = Files.list(path);
+
+        System.out.printf("\tAll the files:\n");
+        test.sorted().map(pathName-> pathName.getFileName().toString()).forEach(v -> System.out.printf("%s\n", v));
+        System.out.printf("-----------------------------\n");
+
+        System.out.printf("\tAll the files ending with .pdf:\n");
+        test1.sorted().map(Path::getFileName).map(Path::toString).filter(s->s.endsWith(".pdf")).forEach(v -> System.out.printf("%s\n", v));
+        System.out.printf("-----------------------------\n");
+
+        System.out.printf("\tFiles sorted by extension:\n");
+        entries.sorted().filter(v->!v.toFile().isDirectory()).map(Path::getFileName).map(Path::toString).
+                collect(Collectors.groupingBy(extension -> extension.substring(extension.lastIndexOf(".")+1), Collectors.toList())).
+                forEach((v, s) ->System.out.printf("%s:\n\t%s\n",v,s));
+//                {
+//                    System.out.printf("%s\n",v);
+//                    test.sorted().filter(value->!value.toFile().isDirectory()).map(Path::getFileName).map(Path::toString).map(p->p.substring(p.lastIndexOf(".") + 1))
+//                            .forEach(t-> System.out.printf("%s\n",t));
+//                });
+
+
+    }
+}
+
