aca21d68e4ce028479488cc9ce40c05563f19f0a
HP Alexei <mailverbitski@gmail.com>
2018-05-02 12:25:41 +0300

    New

--- spring-demo-mvc/src/com/luv2code/springdemo/mvc/Customer.java
+++ spring-demo-mvc/src/com/luv2code/springdemo/mvc/Customer.java
@@ -11,45 +11,44 @@ import org.springframework.lang.NonNull;
 import com.sun.istack.internal.NotNull;
 
 public class Customer {
-	@javax.validation.constraints.NotNull(message="field should be populated")
-	@Length(min=1,max=20,message="enter between 1 and 20 characters")
+	@javax.validation.constraints.NotNull(message = "field should be populated")
+	@Length(min = 1, max = 20, message = "enter between 1 and 20 characters")
 	String firstName;
-	
-	@javax.validation.constraints.NotNull(message="field should be populated")
-	@Length(min=1,max=20,message="enter between 1 and 20 characters")
+
+	@javax.validation.constraints.NotNull(message = "field should be populated")
+	@Length(min = 1, max = 20, message = "enter between 1 and 20 characters")
 	String lasttName;
-	
-	@javax.validation.constraints.NotNull(message="required field")
-	@Min(value=0, message="the number can't be lower than 0")
-	@Max(value=10, message="the number can't be higher  than 10")
+
+	@javax.validation.constraints.NotNull(message = "required field")
+	@Min(value = 0, message = "the number can't be lower than 0")
+	@Max(value = 10, message = "the number can't be higher  than 10")
 	Integer passes;
-	
+
 	@NotBlank()
-//	@javax.validation.constraints.NotNull(message="required field")
-	@Pattern(regexp="^[a-zA-Z0-9] {5}", message="only 5 digts/numbers allowed")
+	// @javax.validation.constraints.NotNull(message="required field")
+	@Pattern(regexp = "^[a-zA-Z0-9] {5}", message = "only 5 digts/numbers allowed")
 	String postalCode;
-	
 
 	public String getFirstName() {
 		return firstName;
 	}
-	
-		public void setFirstName(String firstName) {
+
+	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLasttName() {
 		return lasttName;
 	}
-	
-	
+
 	public void setLasttName(String lasttName) {
 		this.lasttName = lasttName;
 	}
+
 	public Integer getPasses() {
 		return passes;
 	}
-	
-	
+
 	public void setPasses(Integer passes) {
 		this.passes = passes;
 	}
@@ -61,9 +60,5 @@ public class Customer {
 	public void setPostalCode(String postalCode) {
 		this.postalCode = postalCode;
 	}
-	
-	
-	
 
-	
 }
